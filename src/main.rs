a5d5a309c37d9ab73365ecf6278daeb35824836c
Armandas <argnow12@gmail.com>
2020-05-13 00:14:04 +0300

    ai wip

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,12 @@ fn main() {
 
     loop {
         count += 1;
-        make_move(&mut board);
+        make_move_human(&mut board);
+        let (_, pos) = minimax(&board, dim, true, 2);
+        board[pos as usize] = 2;
+        println!("{}", pos);
         draw_grid(&mut board, &dim);
-        let have_winner = check_winner(&board, dim);
+        let have_winner = check_winner(&board, dim, 1) || check_winner(&board, dim, 2);
         if have_winner {
             println!("Winner winner chicken dinner!");
             break;
@@ -27,22 +30,22 @@ fn main() {
 }
 
 // TODO: seperate checkers to their own functions;
-fn check_winner(board: &Vec<u32>, size: u32) -> bool {
+fn check_winner(board: &Vec<u32>, size: u32, player: u32) -> bool {
     for i in 0..size {
         let mut matched_column = 0;
         let mut matched_row = 0;
 
         for j in 0..size {
-            if board[(i * size + j) as usize] == 1 {
+            if board[(i * size + j) as usize] == player {
                 matched_row += 1;
             }
 
-            if board[(j * size + i) as usize] == 1 {
+            if board[(j * size + i) as usize] == player {
                 matched_column += 1;
             }
         }
 
-        if matched_row == 3 || matched_column == 3 {
+        if matched_row == size || matched_column == size {
             return true;
         }
     }
@@ -52,14 +55,14 @@ fn check_winner(board: &Vec<u32>, size: u32) -> bool {
         let mut sum = index;
         if *value == 1 {
             for _i in 0..size {
-                if board.len() - 1 >= sum && board[sum as usize] == 1 {
+                if board.len() - 1 >= sum && board[sum as usize] == player {
                     matched_diagnal += 1;
                 }
                 sum += (size + 1) as usize;
             }
         }
 
-        if matched_diagnal == 3 {
+        if matched_diagnal == size {
             return true;
         }
     }
@@ -67,7 +70,41 @@ fn check_winner(board: &Vec<u32>, size: u32) -> bool {
     false
 }
 
-fn make_move(board: &mut Vec<u32>) {
+fn minimax(board: &Vec<u32>, size: u32, is_maximizing: bool, player: u32) -> (i32, u32) {
+    let mut winning_position = 0;
+    if check_winner(&board, size, player) {
+        return (1, winning_position);
+    }
+
+    let mut board_clone = board.clone();
+    let mut score = 0;
+    for i in 0..board_clone.len() {
+        if board_clone[i] == 0 {
+            board_clone[i] = 2;
+            let (mut best_score, _) = minimax(
+                &board_clone,
+                size,
+                if is_maximizing { false } else { true },
+                player,
+            );
+
+            if !is_maximizing {
+                best_score = -best_score;
+            }
+
+            if best_score > score {
+                score = best_score;
+                winning_position = i as u32;
+            }
+        }
+    }
+
+    (score, winning_position)
+}
+
+fn make_smart_move(board: &mut Vec<u32>) {}
+
+fn make_move_human(board: &mut Vec<u32>) {
     println!("Please input your a position number that you want to check");
 
     let mut position = String::new();
